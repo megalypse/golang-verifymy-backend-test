fbba2bc113e2649e405ccbc86ed3e93ee25a87ba
Bruno Alliguieri <devalliguieri@gmail.com>
2023-03-21 11:50:19 -0300

    ref: base entity composition changed

--- internal/domain/models/base_entity.go
+++ internal/domain/models/base_entity.go
@@ -3,8 +3,24 @@ package models
 import "time"
 
 type BaseEntity struct {
-	Id        int64
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt time.Time
+	Identity
+	CreatedAt
+	UpdatedAt
+	DeletedAt
+}
+
+type Identity struct {
+	Id int64 `json:"id"`
+}
+
+type CreatedAt struct {
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type UpdatedAt struct {
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type DeletedAt struct {
+	DeletedAt time.Time `json:"deleted_at"`
 }
