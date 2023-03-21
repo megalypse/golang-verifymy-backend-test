16f648404b329626a6b4698661ff13df05b7d540
Bruno Alliguieri <devalliguieri@gmail.com>
2023-03-20 22:06:23 -0300

    feat: user controller

---
+++ internal/presentation/http/controllers/usercontroller/user_controller.go
@@ -0,0 +1,40 @@
+package usercontroller
+
+import (
+	"net/http"
+
+	"github.com/megalypse/golang-verifymy-backend-test/internal/domain/usecases/usecases"
+	"github.com/megalypse/golang-verifymy-backend-test/internal/presentation/http/controllers"
+)
+
+type UserController struct {
+	CreateUserUsecase   usecases.CreateUser
+	UpdateUserUsecase   usecases.UpdateUser
+	DeleteUserUsecase   usecases.DeleteUser
+	FindUserByIdUsecase usecases.FindUserById
+}
+
+func (uc UserController) GetHandlers() []controllers.RouteDefinition {
+	return []controllers.RouteDefinition{
+		{
+			Method:       http.MethodPost,
+			Route:        "/user",
+			HandlingFunc: uc.createUser,
+		},
+		{
+			Method:       http.MethodPut,
+			Route:        "/user",
+			HandlingFunc: uc.updateUser,
+		},
+		{
+			Method:       http.MethodDelete,
+			Route:        "/user/{userId}",
+			HandlingFunc: uc.deleteUser,
+		},
+		{
+			Method:       http.MethodGet,
+			Route:        "/user/{userId}",
+			HandlingFunc: uc.deleteUser,
+		},
+	}
+}
